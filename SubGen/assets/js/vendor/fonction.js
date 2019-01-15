351b2d8b37dc6efa8597fee465ecacfda0d6e6a2
Quentin2B <35448908+Quentin2B@users.noreply.github.com>
2019-01-15 11:45:33 +0100

    SubGen

--- SubGen/assets/js/vendor/fonction.js
+++ SubGen/assets/js/vendor/fonction.js
@@ -1,29 +1,61 @@
-'use strict';
-
-;( function ( document, window, index )
+var inputs = document.querySelectorAll( '.inputfile' );
+Array.prototype.forEach.call( inputs, function( input )
 {
-	var inputs = document.querySelectorAll( '.inputfile' );
-	Array.prototype.forEach.call( inputs, function( input )
+	var label	 = input.nextElementSibling,
+		labelVal = label.innerHTML;
+
+	input.addEventListener( 'change', function( e )
 	{
-		var label	 = input.nextElementSibling,
-			labelVal = label.innerHTML;
+		var fileName = '';
+		if( this.files && this.files.length > 1 )
+			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+		else
+			fileName = e.target.value.split( '\\' ).pop();
 
-		input.addEventListener( 'change', function( e )
-		{
-			var fileName = '';
-			if( this.files && this.files.length > 1 )
-				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-			else
-				fileName = e.target.value.split( '\\' ).pop();
-
-			if( fileName )
-				label.querySelector( 'span' ).innerHTML = fileName;
-			else
-				label.innerHTML = labelVal;
-		});
-
-		// Firefox bug fix
-		input.addEventListener( 'focus', function(){ input.classList.add( 'has-focus' ); });
-		input.addEventListener( 'blur', function(){ input.classList.remove( 'has-focus' ); });
+		if( fileName )
+			label.querySelector( 'span' ).innerHTML = fileName;
+		else
+			label.innerHTML = labelVal;
 	});
-}( document, window, 0 ));
+});
+
+function envURL(){
+
+	var notreURL;
+
+	notreURL = document.getElementById("URL").value;
+
+	alert(notreURL);
+}
+
+function _(el){
+    return document.getElementById(el);
+}
+function uploadFile(){
+    var file = _("file").files[0];
+    var formdata = new FormData();
+    formdata.append("file", file);
+    var ajax = new XMLHttpRequest();
+    ajax.upload.addEventListener("progress", progressHandler, false);
+    ajax.addEventListener("load", completeHandler, false);
+    ajax.addEventListener("error", errorHandler, false);
+    ajax.addEventListener("abort", abortHandler, false);
+    ajax.open("POST", "file_upload_parser.php");
+    ajax.send(formdata);
+}
+function progressHandler(event){
+    _("loaded_n_total").innerHTML = "Uploaded "+event.loaded+" bytes of "+event.total;
+    var percent = (event.loaded / event.total) * 100;
+    _("progressBar").value = Math.round(percent);
+    _("status").innerHTML = Math.round(percent)+"% uploaded... please wait";
+}
+function completeHandler(event){
+    _("status").innerHTML = event.target.responseText;
+    _("progressBar").value = 0;
+}
+function errorHandler(event){
+    _("status").innerHTML = "Upload Failed";
+}
+function abortHandler(event){
+    _("status").innerHTML = "Upload Aborted";
+}
